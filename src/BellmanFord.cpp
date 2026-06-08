e10b57b2891ca050a0ba84e358bec353dc825f69
TheoCamposSoares <theocampossoares12@gmail.com>
2026-06-08 13:28:42 -0300

    Função imprimirCaminho movida de lugar

--- src/BellmanFord.cpp
+++ src/BellmanFord.cpp
@@ -61,28 +61,3 @@ bool cicloNegativo(int numVertices, const vector<vector<pair<int, int>>>& listaA
     }
     return true;
 }
-
-void imprimirCaminho(int origem, int destino, const vector<int>& predecessores) {
-    if(predecessores[destino] == -1 && origem != destino) {
-        cout << "ALERTA: Não há caminho viável até o destino!\n";
-        return;
-    }
-
-    cout << "Rota: ";
-
-    if(origem != destino) {
-        construirCaminho(destino, predecessores);
-    } else {
-        cout << origem;
-    }
-}
-
-void construirCaminho(int verticeAtual, const vector<int>& predecessores) {
-    if(predecessores[verticeAtual] == -1) {
-        cout << verticeAtual;
-        return;
-    }
-
-    construirCaminho(predecessores[verticeAtual], predecessores);
-    cout << " -> " << verticeAtual;
-}
